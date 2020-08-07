a60a266b60031c6e99493ce878f39b2a8aa34e33
DaniW42 <assgex@gmail.com>
2020-08-07 23:48:24 +0200

    complete switch to variables based on conf file

--- bin/motion-send.sh
+++ bin/motion-send.sh
@@ -1,38 +1,40 @@
 #!/bin/bash
 
 ## Read Configuration-File so we have the variables available
-. telegram.conf
+source ../telegram.conf
 
-printf "=========================================================\n" >> /home/pi/motion-send.log
-printf "$(date) - MOTION DETECTED.\n" >> /home/pi/motion-send.log
+printf "=========================================================\n" >> $var_gitDir/motion-send.log
+printf "$(date) - MOTION DETECTED.\n" >> $var_gitDir/motion-send.log
 
-<<<<<<< HEAD
-if ping -c 3 -W 1000 $var_pingAddress > /dev/null
-=======
-if ping -c 3 -W 2 192.168.0.210 > /dev/null
->>>>>>> 13e00c5c2ad74aa1988fc28932571f732fec445b
+if ping -c 3 -W 2 $var_pingAddress > /dev/null
 
 then
-	printf "$(date) - OP6T available, end script.\n" >> /home/pi/motion-send.log
-	printf "=========================================================\n" >> /home/pi/motion-send.log
-	printf " \n" >> /home/pi/motion-send.log
+	printf "$(date) - Device available, end script.\n" >> $var_gitDir/motion-send.log
+	printf "=========================================================\n" >> $var_gitDir/motion-send.log
+	printf " \n" >> $var_gitDir/motion-send.log
 else
-	printf "$(date) - OP6T NOT available, RUN script.\n" >> /home/pi/motion-send.log
+	printf "$(date) - Device NOT available, RUN script.\n" >> $var_gitDir/motion-send.log
 
-	lastsnap=$(tree -ftri /mnt/hdd/shared/motioneye/ | grep .jpg | head -n1)
-	lastvideo=$(tree -ftri /mnt/hdd/shared/motioneye/ | grep -v thumb | grep .mp4 | head -n1)
+	lastsnap=$(tree -ftri "$var_mediaDir" | grep .jpg | head -n1)
+	lastvideo=$(tree -ftri "$var_mediaDir" | grep -v thumb | grep .mp4 | head -n1)
 
-	printf "$(date) - \$lastsnap is $lastsnap\n" >> /home/pi/motion-send.log
-	printf "$(date) - \$lastvideo is $lastvideo\n" >> /home/pi/motion-send.log
-	printf "$(date) - BEGIN tg-sendphoto.py:\n" >> /home/pi/motion-send.log
-	printf " \n" >> /home/pi/motion-send.log
+	printf "$(date) - \$lastsnap is $lastsnap\n" >> $var_gitDir/motion-send.log
+	printf "$(date) - \$lastvideo is $lastvideo\n" >> $var_gitDir/motion-send.log
 
-	/bin/tg-sendPhoto.py $lastsnap "Bewegung erkannt! - ${lastsnap: -23:19}" >> /home/pi/motion-send.log
-	/bin/tg-sendVideo.py $lastvideo "Bewegung erkannt! - ${lastvideo: -23:19}" >> /home/pi/motion-send.log
+	printf "$(date) - BEGIN tg-sendphoto.py:\n" >> $var_gitDir/motion-send.log
+	printf " \n" >> $var_gitDir/motion-send.log
+	$var_gitDir/bin/tg-sendPhoto.py $var_botApiKey $var_chatId $lastsnap "Bewegung erkannt! - ${lastsnap: -23:19}" >> $var_gitDir/motion-send.log
+	printf " \n" >> $var_gitDir/motion-send.log
+	printf " \n" >> $var_gitDir/motion-send.log
+	printf "$(date) - END tg-sendphoto.py.\n" >> $var_gitDir/motion-send.log
 
-	printf " \n" >> /home/pi/motion-send.log
-	printf " \n" >> /home/pi/motion-send.log
-	printf "$(date) - END tg-sendphoto.py.\n" >> /home/pi/motion-send.log
-	printf "=========================================================\n" >> /home/pi/motion-send.log
-	printf " \n" >> /home/pi/motion-send.log
+	printf "$(date) - BEGIN tg-sendVideo.py:\n" >> $var_gitDir/motion-send.log
+	printf " \n" >> $var_gitDir/motion-send.log
+	$var_gitDir/bin/tg-sendVideo.py $var_botApiKey $var_chatId $lastvideo "Bewegung erkannt! - ${lastvideo: -23:19}" >> $var_gitDir/motion-send.log
+	printf " \n" >> $var_gitDir/motion-send.log
+	printf " \n" >> $var_gitDir/motion-send.log
+	printf "$(date) - END tg-sendVideo.py.\n" >> $var_gitDir/motion-send.log
+
+	printf "=========================================================\n" >> $var_gitDir/motion-send.log
+	printf " \n" >> $var_gitDir/motion-send.log
 fi
