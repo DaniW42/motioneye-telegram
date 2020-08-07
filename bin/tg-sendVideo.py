9ce77802636f17789b2f1146af93e8b0799ee5da
DaniW42 <assgex@gmail.com>
2020-08-07 23:47:29 +0200

    fixed curl command after switch to varaiable based execution

--- bin/tg-sendVideo.py
+++ bin/tg-sendVideo.py
@@ -1,2 +1,2 @@
-curl -s -X POST "https://api.telegram.org/bot"$1"/sendVideo" -F chat_id="$2" -F video="@$1" -F caption="$2"
+curl -s -X POST "https://api.telegram.org/bot"$1"/sendVideo" -F chat_id="$2" -F video="@$3" -F caption="$4"
 
