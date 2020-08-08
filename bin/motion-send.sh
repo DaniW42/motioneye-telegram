d02cf56174f6efaa5813a8e8462cf95985729230
DaniW42 <assgex@gmail.com>
2020-08-08 02:37:24 +0200

    update to use variables and functions

--- bin/motion-send.sh
+++ bin/motion-send.sh
@@ -1,40 +1,51 @@
 #!/bin/bash
 
-## Read Configuration-File so we have the variables available
-source ../telegram.conf
+## get my current directory
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-printf "=========================================================\n" >> $var_gitDir/motion-send.log
-printf "$(date) - MOTION DETECTED.\n" >> $var_gitDir/motion-send.log
+## Read Configuration-File so we have the variables available
+source $DIR/../telegram.conf
+
+## Get the name of the camera which detected a motion (done by parameter %$ in motioneye webinterface command)
+var_cameraName=$1
+
+## Functions
+func_writeLog () {
+   printf "$1\n" >> $var_gitDir/motion-send.log
+}
+
+func_sendPhoto () {
+   $var_gitDir/bin/tg-sendPhoto.py $var_botApiKey $var_chatId $1 $2
+}
+
+func_sendVideo () {
+   $var_gitDir/bin/tg-sendVideo.py $var_botApiKey $var_chatId $1 $2
+}
+
+func_writeLog "========================================================="
+func_writeLog "$(date) - MOTION DETECTED by Camera $var_cameraName."
 
 if ping -c 3 -W 2 $var_pingAddress > /dev/null
 
 then
-	printf "$(date) - Device available, end script.\n" >> $var_gitDir/motion-send.log
-	printf "=========================================================\n" >> $var_gitDir/motion-send.log
-	printf " \n" >> $var_gitDir/motion-send.log
+	func_writeLog "$(date) - Device available, end script."
+	func_writeLog "========================================================="
+	func_writeLog ""
 else
-	printf "$(date) - Device NOT available, RUN script.\n" >> $var_gitDir/motion-send.log
+	func_writeLog "$(date) - Device NOT available, RUN script."
 
 	lastsnap=$(tree -ftri "$var_mediaDir" | grep .jpg | head -n1)
 	lastvideo=$(tree -ftri "$var_mediaDir" | grep -v thumb | grep .mp4 | head -n1)
+	func_writeLog "$(date) - \$lastsnap is $lastsnap"
+	func_writeLog "$(date) - \$lastvideo is $lastvideo"
 
-	printf "$(date) - \$lastsnap is $lastsnap\n" >> $var_gitDir/motion-send.log
-	printf "$(date) - \$lastvideo is $lastvideo\n" >> $var_gitDir/motion-send.log
-
-	printf "$(date) - BEGIN tg-sendphoto.py:\n" >> $var_gitDir/motion-send.log
-	printf " \n" >> $var_gitDir/motion-send.log
-	$var_gitDir/bin/tg-sendPhoto.py $var_botApiKey $var_chatId $lastsnap "Bewegung erkannt! - ${lastsnap: -23:19}" >> $var_gitDir/motion-send.log
-	printf " \n" >> $var_gitDir/motion-send.log
-	printf " \n" >> $var_gitDir/motion-send.log
-	printf "$(date) - END tg-sendphoto.py.\n" >> $var_gitDir/motion-send.log
-
-	printf "$(date) - BEGIN tg-sendVideo.py:\n" >> $var_gitDir/motion-send.log
-	printf " \n" >> $var_gitDir/motion-send.log
-	$var_gitDir/bin/tg-sendVideo.py $var_botApiKey $var_chatId $lastvideo "Bewegung erkannt! - ${lastvideo: -23:19}" >> $var_gitDir/motion-send.log
-	printf " \n" >> $var_gitDir/motion-send.log
-	printf " \n" >> $var_gitDir/motion-send.log
-	printf "$(date) - END tg-sendVideo.py.\n" >> $var_gitDir/motion-send.log
-
-	printf "=========================================================\n" >> $var_gitDir/motion-send.log
-	printf " \n" >> $var_gitDir/motion-send.log
+	func_writeLog "$(date) - BEGIN tg-sendPhoto.py:"
+	$var_gitDir/bin/tg-sendPhoto.py $var_botApiKey $var_chatId $lastsnap "$var_cameraName - ${lastsnap: -23:19}"
+	func_writeLog "$(date) - BEGIN tg-sendVideo.py:"
+	$var_gitDir/bin/tg-sendVideo.py $var_botApiKey $var_chatId $lastvideo "$var_cameraName - ${lastsnap: -23:19}"
+	
+	func_writeLog "========================================================="
+	func_writeLog ""
 fi
+
+exit 0
