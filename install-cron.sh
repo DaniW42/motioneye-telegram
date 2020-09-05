e719303e9e0b258dee1888095ba9c63f4608d5f7
DaniW42 <assgex@gmail.com>
2020-09-06 00:09:23 +0200

    added language selector

--- install-cron.sh
+++ install-cron.sh
@@ -14,11 +14,39 @@ func_writeConsole () {
 	printf '%s\n' "$1" >> $var_logFile
 }
 
+func_chooseLanguage () {
+	var_languages=($(ls -1 $var_scriptDir/languages/ | sed -e 's/\.lang$//'))
+	counter=0
+	
+	echo ""
+	echo "Choose your language:"
+	for i in "${var_languages[@]}"
+	do
+	:
+	echo "$counter: $i"
+	counter=$[counter + 1]
+	done
+	
+	echo ""
+	
+	
+	var_defaultLanguage="0"
+	read -p "Which language do you prefer? (Default is ${var_languages[$var_defaultLanguage]}): " var_userChoice
+	: ${var_userChoice:=$var_defaultLanguage}
+	source $var_scriptDir/languages/${var_languages[$var_userChoice]}".lang"
+	
+	echo $var_chosenLanguage
+	
+	echo ""
+}
+
+func_chooseLanguage
+
 if [[ "$EUID" -ne 0 ]]; then
-  func_writeConsole "Please run as root"
+  func_writeConsole "$var_runAsRootText"
   exit 1
 else
-  func_writeConsole "\n\nAbout to install /etc/cron.d/motioneye-telegram with content."
+  func_writeConsole "$var_startInstallCronText"
   func_writeLog ""
   func_writeLog "##################################################################"
   func_writeLog "* * * * * root $var_scriptDir/bin/presencecheck.sh >/dev/null 2>&1"
@@ -26,26 +54,26 @@ else
   func_writeLog ""
 
   var_defaultNewCron="y"
-  read -p "Create new cronjob? [Y/n]: " var_NewCron
+  read -p "$var_createCronjobText" var_NewCron
   : ${var_NewCron:=$var_defaultNewCron}
 
   case $var_NewCron in
   ## [y] create new config file or [n] simply quit.
     [yY] | [yY][eE][sS] )
-      func_writeConsole "Ok, I will create new cronjob."
+      func_writeConsole "$var_startCreatingCronjobText"
       touch $var_cronFile
       echo "* * * * * root $var_scriptDir/bin/presencecheck.sh >/dev/null 2>&1" > $var_cronFile
-      test $var_cronFile && func_writeConsole "Successful, end script."; exit 0 || func_writeConsole "Hmmm.. Something went wrong, no file created..."; exit 1
+      test $var_cronFile && func_writeConsole "$var_successfullText"; exit 0 || func_writeConsole "$var_errorText2"; exit 1
       ;;
 
     [nN] | [nN][oO] )
-      func_writeConsole "You selected to not create new cronjob."
-      func_writeConsole "That is Ok, but you have to do this on your own. See ya!"
+      func_writeConsole "$var_selectedNoText"
+      func_writeConsole "$var_installItYourselfText"
       exit 0
       ;;
 
     *)
-      func_writeConsole "Wrong answer given. Abort install."
+      func_writeConsole "$var_wrongInputText"
       exit 1
       ;;
   esac
