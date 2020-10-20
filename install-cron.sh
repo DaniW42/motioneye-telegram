46f1057deb23a559c0c8802f90640469b8832b1c
Dani <assgex@gmail.com>
2020-10-20 15:50:14 +0200

    added comments for readbility

--- install-cron.sh
+++ install-cron.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 
+## check for root privileges
 if [[ "$EUID" -ne 0 ]]; then
   echo "Please run as root" >&2
   exit 1
 fi
 
+## get current path to be set as directory in cron
 var_scriptDir="$(
 	cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null \
 	&& pwd
 )"
 
+## where we place the cron job
 var_cronFile="/etc/cron.d/motioneye-telegram"
 var_defaultNewCron="y"
 
@@ -21,11 +24,12 @@ cat <<-INTRO
 
 INTRO
 
-
+## ask user to press corresponding key
 read -n1 -p "Create new cronjob? [Y/n]: " var_NewCron
 : ${var_NewCron:=$var_defaultNewCron}
 echo
 
+## run action based on users choice
 case ${var_NewCron,,} in
 	## [y] create new config file or [n] simply quit.
 	y)
@@ -33,6 +37,7 @@ case ${var_NewCron,,} in
 		touch $var_cronFile
 		echo "* * * * * root $var_scriptDir/bin/presencecheck.sh &>/dev/null" > $var_cronFile
 
+		## check if file was created and either exit or report error
 		test $var_cronFile && echo "Successful, end script."
 		exit 0 || echo "Hmmm.. Something went wrong, no file created...";
 		exit 1
